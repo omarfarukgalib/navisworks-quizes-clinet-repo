320ba04730a3cc236a955119af73a7db5c482d80
omarfarukgalib <galib.omar98@gmail.com>
2022-10-12 01:30:41 +0600

    404 route are added

--- src/components/ReactQuiz/ReactQuiz.js
+++ src/components/ReactQuiz/ReactQuiz.js
@@ -5,25 +5,25 @@ const ReactQuiz = () => {
     const quiz = useLoaderData()
     const {id,name,question,options} = quiz;
     return (
-        <div className='mt-3'>
+        <div className='mt-3 bg-red-200 text-xl font-semibold'>
             
              
              <h2>Quiz 1:  {quiz.data.questions[0].question} 
            <div className='grid grid-cols-2 gap-3 mt-3 px-52 '>
-           <h2 className='border-2 rounded-md p-3'>
+           <h2 className='border-2 border-black rounded-md p-3'>
            <input className='mr-2 ' type="radio" value="Male" name="gender" />
              a. {quiz.data.questions[0].options[0]} 
                 </h2> 
-                <h2 className='border-2 rounded-md p-3'>
+                <h2 className='border-2 border-black rounded-md p-3'>
                 <input className='mr-2 ' type="radio" value="Male" name="gender" />
                 b. {quiz.data.questions[0].options[1]} 
                 </h2>
-                <h2 className='border-2 rounded-md p-3'>
+                <h2 className='border-2 border-black rounded-md p-3'>
                 <input className='mr-2 ' type="radio" value="Male" name="gender" />
                 c. {quiz.data.questions[0].options[2]} 
              
                 </h2>
-                <h2 className='border-2 rounded-md p-3'>
+                <h2 className='border-2 border-black rounded-md p-3'>
                 <input className='mr-2 ' type="radio" value="Male" name="gender" />
                 d. {quiz.data.questions[0].options[3]}
                 </h2>
@@ -32,20 +32,20 @@ const ReactQuiz = () => {
              
              <h2 className='mt-12'>Quiz 2:  {quiz.data.questions[1].question} 
            <div className='grid grid-cols-2 gap-3 mt-3 px-52 '>
-           <h2 className='border-2 rounded-md p-3'>
+           <h2 className='border-2 border-black rounded-md p-3'>
            <input className='mr-2 ' type="radio" value="Male" name="gender" />
              a. {quiz.data.questions[1].options[0]} 
                 </h2> 
-                <h2 className='border-2 rounded-md p-3'>
+                <h2 className='border-2 border-black rounded-md p-3'>
                 <input className='mr-2 ' type="radio" value="Male" name="gender" />
                 b. {quiz.data.questions[1].options[1]} 
                 </h2>
-                <h2 className='border-2 rounded-md p-3'>
+                <h2 className='border-2 border-black rounded-md p-3'>
                 <input className='mr-2 ' type="radio" value="Male" name="gender" />
                 c. {quiz.data.questions[1].options[2]} 
              
                 </h2>
-                <h2 className='border-2 rounded-md p-3 '>
+                <h2 className='border-2 border-black rounded-md p-3 '>
                 <input className='mr-2 ' type="radio" value="Male" name="gender" />
                 d. {quiz.data.questions[1].options[3]}
                 </h2>
@@ -53,25 +53,152 @@ const ReactQuiz = () => {
              </h2>
              <h2 className='mt-12'>Quiz 3:  {quiz.data.questions[2].question} 
            <div className='grid grid-cols-2 gap-3 mt-3 px-52 '>
-           <h2 className='border-2 rounded-md p-3'>
+           <h2 className='border-2 border-black rounded-md p-3'>
            <input className='mr-2 ' type="radio" value="Male" name="gender" />
              a. {quiz.data.questions[2].options[0]} 
                 </h2> 
-                <h2 className='border-2 rounded-md p-3'>
+                <h2 className='border-2 border-black rounded-md p-3'>
                 <input className='mr-2 ' type="radio" value="Male" name="gender" />
                 b. {quiz.data.questions[2].options[1]} 
                 </h2>
-                <h2 className='border-2 rounded-md p-3'>
+                <h2 className='border-2 border-black rounded-md p-3'>
                 <input className='mr-2 ' type="radio" value="Male" name="gender" />
                 c. {quiz.data.questions[2].options[2]} 
              
                 </h2>
-                <h2 className='border-2 rounded-md p-3 '>
+                <h2 className='border-2 border-black rounded-md p-3 '>
                 <input className='mr-2 ' type="radio" value="Male" name="gender" />
                 d. {quiz.data.questions[2].options[3]}
                 </h2>
            </div>
              </h2>
+             <h2 className='mt-12'>Quiz 4:  {quiz.data.questions[3].question} 
+           <div className='grid grid-cols-2 gap-3 mt-3 px-52 '>
+           <h2 className='border-2 border-black rounded-md p-3'>
+           <input className='mr-2 ' type="radio" value="Male" name="gender" />
+             a. {quiz.data.questions[3].options[0]} 
+                </h2> 
+                <h2 className='border-2 border-black rounded-md p-3'>
+                <input className='mr-2 ' type="radio" value="Male" name="gender" />
+                b. {quiz.data.questions[3].options[1]} 
+                </h2>
+                <h2 className='border-2 border-black rounded-md p-3'>
+                <input className='mr-2 ' type="radio" value="Male" name="gender" />
+                c. {quiz.data.questions[3].options[2]} 
+             
+                </h2>
+                <h2 className='border-2 border-black rounded-md p-3 '>
+                <input className='mr-2 ' type="radio" value="Male" name="gender" />
+                d. {quiz.data.questions[3].options[3]}
+                </h2>
+           </div>
+             </h2>
+             <h2 className='mt-12'>Quiz 5:  {quiz.data.questions[4].question} 
+           <div className='grid grid-cols-2 gap-3 mt-3 px-52 '>
+           <h2 className='border-2 border-black rounded-md p-3'>
+           <input className='mr-2 ' type="radio" value="Male" name="gender" />
+             a. {quiz.data.questions[4].options[0]} 
+                </h2> 
+                <h2 className='border-2 border-black rounded-md p-3'>
+                <input className='mr-2 ' type="radio" value="Male" name="gender" />
+                b. {quiz.data.questions[4].options[1]} 
+                </h2>
+                <h2 className='border-2 border-black rounded-md p-3'>
+                <input className='mr-2 ' type="radio" value="Male" name="gender" />
+                c. {quiz.data.questions[4].options[2]} 
+             
+                </h2>
+                <h2 className='border-2 border-black rounded-md p-3 '>
+                <input className='mr-2 ' type="radio" value="Male" name="gender" />
+                d. {quiz.data.questions[4].options[3]}
+                </h2>
+           </div>
+             </h2>
+             <h2 className='mt-12'>Quiz 6:  {quiz.data.questions[5].question} 
+           <div className='grid grid-cols-2 gap-3 mt-3 px-52 '>
+           <h2 className='border-2 border-black rounded-md p-3'>
+           <input className='mr-2 ' type="radio" value="Male" name="gender" />
+             a. {quiz.data.questions[5].options[0]} 
+                </h2> 
+                <h2 className='border-2 border-black rounded-md p-3'>
+                <input className='mr-2 ' type="radio" value="Male" name="gender" />
+                b. {quiz.data.questions[5].options[1]} 
+                </h2>
+                <h2 className='border-2 border-black rounded-md p-3'>
+                <input className='mr-2 ' type="radio" value="Male" name="gender" />
+                c. {quiz.data.questions[5].options[2]} 
+             
+                </h2>
+                <h2 className='border-2 border-black rounded-md p-3 '>
+                <input className='mr-2 ' type="radio" value="Male" name="gender" />
+                d. {quiz.data.questions[5].options[3]}
+                </h2>
+           </div>
+             </h2>
+             <h2 className='mt-12'>Quiz 6:  {quiz.data.questions[5].question} 
+           <div className='grid grid-cols-2 gap-3 mt-3 px-52 '>
+           <h2 className='border-2 border-black rounded-md p-3'>
+           <input className='mr-2 ' type="radio" value="Male" name="gender" />
+             a. {quiz.data.questions[5].options[0]} 
+                </h2> 
+                <h2 className='border-2 border-black rounded-md p-3'>
+                <input className='mr-2 ' type="radio" value="Male" name="gender" />
+                b. {quiz.data.questions[5].options[1]} 
+                </h2>
+                <h2 className='border-2 border-black rounded-md p-3'>
+                <input className='mr-2 ' type="radio" value="Male" name="gender" />
+                c. {quiz.data.questions[5].options[2]} 
+             
+                </h2>
+                <h2 className='border-2 border-black rounded-md p-3 '>
+                <input className='mr-2 ' type="radio" value="Male" name="gender" />
+                d. {quiz.data.questions[5].options[3]}
+                </h2>
+           </div>
+             </h2>
+             <h2 className='mt-12'>Quiz 7:  {quiz.data.questions[6].question} 
+           <div className='grid grid-cols-2 gap-3 mt-3 px-52 '>
+           <h2 className='border-2 border-black rounded-md p-3'>
+           <input className='mr-2 ' type="radio" value="Male" name="gender" />
+             a. {quiz.data.questions[6].options[0]} 
+                </h2> 
+                <h2 className='border-2 border-black rounded-md p-3'>
+                <input className='mr-2 ' type="radio" value="Male" name="gender" />
+                b. {quiz.data.questions[6].options[1]} 
+                </h2>
+                <h2 className='border-2 border-black rounded-md p-3'>
+                <input className='mr-2 ' type="radio" value="Male" name="gender" />
+                c. {quiz.data.questions[6].options[2]} 
+             
+                </h2>
+                <h2 className='border-2 border-black rounded-md p-3 '>
+                <input className='mr-2 ' type="radio" value="Male" name="gender" />
+                d. {quiz.data.questions[6].options[3]}
+                </h2>
+           </div>
+             </h2>
+             <h2 className='mt-12'>Quiz 8:  {quiz.data.questions[7].question} 
+           <div className='grid grid-cols-2 border-black gap-3 mt-3 px-52 '>
+           <h2 className='border-2 border-black rounded-md p-3'>
+           <input className='mr-2 ' type="radio" value="Male" name="gender" />
+             a. {quiz.data.questions[7].options[0]} 
+                </h2> 
+                <h2 className='border-2 border-black rounded-md p-3'>
+                <input className='mr-2 ' type="radio" value="Male" name="gender" />
+                b. {quiz.data.questions[7].options[1]} 
+                </h2>
+                <h2 className='border-2 border-black rounded-md p-3'>
+                <input className='mr-2 ' type="radio" value="Male" name="gender" />
+                c. {quiz.data.questions[7].options[2]} 
+             
+                </h2>
+                <h2 className='border-2 border-black rounded-md p-3 '>
+                <input className='mr-2 ' type="radio" value="Male" name="gender" />
+                d. {quiz.data.questions[7].options[3]}
+                </h2>
+           </div>
+             </h2>
+            
         </div>
     );
 };
